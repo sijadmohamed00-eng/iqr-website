777c7141940d93ea856047aa8f70fe678c03e343
سجاد محمد صادج <sijadmohamed00@gmail.com>
2026-04-28 05:23:50 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',      // يحول المشروع إلى ملفات ثابتة (HTML/CSS/JS)
+  output: 'export',
   images: {
-    unoptimized: true,   // ضروري لتعمل الصور والأيقونات بدون سيرفر معالجة
+    unoptimized: true,
   },
-  // إذا كنت تستخدم Framer Motion أو Three.js، يفضل ترك الإعدادات الافتراضية أدناه
   reactStrictMode: true,
 }
 
