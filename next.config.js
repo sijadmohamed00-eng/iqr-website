84c96f9e1f2ecab55277bd69b4600995bf6896d5
سجاد محمد صادج <sijadmohamed00@gmail.com>
2026-04-30 19:03:14 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // تفعيل التصدير الثابت للملفات
   output: 'export',
+  // ضمان عمل الروابط مثل /dashboard/ بشكل صحيح
   trailingSlash: true,
+  // إضافة إعدادات إضافية إذا لزم الأمر مستقبلاً
+  reactStrictMode: true,
 };
 
 module.exports = nextConfig;
