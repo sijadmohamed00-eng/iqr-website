cb82ce04bfbeca1258310516e793b65a8c17a8d4
سجاد محمد صادج <sijadmohamed00@gmail.com>
2026-04-30 19:48:30 +0300

    Update page.js

--- app/contact/page.js
+++ app/contact/page.js
@@ -2,7 +2,6 @@ export const dynamic = 'force-static';
 
 export const metadata = {
   title: "تواصل معنا | IQR لإدارة وتطوير المطاعم",
-  description: "نحن هنا لمساعدتك في تطوير مطعمك. تواصل مع فريق IQR و Oxaam في بغداد.",
 };
 
 export { default } from "./ContactClient";
