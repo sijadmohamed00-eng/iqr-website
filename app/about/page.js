296b0e3b5e5f0cd2ff6a501388c2248d9034d3d3
سجاد محمد صادج <sijadmohamed00@gmail.com>
2026-04-30 19:56:54 +0300

    Delete app directory

--- app/about/page.js
+++
@@ -1,7 +0,0 @@
-export const dynamic = 'force-static';
-
-export const metadata = {
-  title: "من نحن | IQR لإدارة المطاعم في العراق",
-};
-
-export { default } from "./AboutClient";
--- app/contact/page.js
+++
@@ -1,7 +0,0 @@
-export const dynamic = 'force-static';
-
-export const metadata = {
-  title: "تواصل معنا | IQR لإدارة وتطوير المطاعم",
-};
-
-export { default } from "./ContactClient";
