6bde6a48962ebd7d12325c1fa23fd2fdd340c2e8
سجاد محمد صادج <sijadmohamed00@gmail.com>
2026-05-04 19:14:01 +0300

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,10 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
-};
-
-module.exports = nextConfig;
+const nextConfig = {}
+module.exports = nextConfig
